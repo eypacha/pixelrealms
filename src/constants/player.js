d4cc67ae4be0a6daea951be0d3c4998e4f9779f7
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-16 11:41:13 -0300

    fix: reorder player constants for better organization

--- src/constants/player.js
+++ src/constants/player.js
@@ -1,6 +1,7 @@
 export const PLAYER_SPEED = 5;
-export const ENCOUNTER_RATE = 0.1;
 export const INITIAL_HEALTH = 10;
 export const INITIAL_STRENGTH = 10;
 export const INITIAL_DEFENSE = 10;
 export const INITIAL_COINS = 0;
+
+export const ENCOUNTER_RATE = 0.1;
