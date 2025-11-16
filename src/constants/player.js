999660d814f82394d072ebc585649ea13075a160
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-16 15:44:29 -0300

    feat: update INITIAL_POTIONS to correct starting value

--- src/constants/player.js
+++ src/constants/player.js
@@ -3,7 +3,7 @@ export const INITIAL_HEALTH = 10;
 export const INITIAL_STRENGTH = 10;
 export const INITIAL_DEFENSE = 10;
 export const INITIAL_COINS = 0;
-export const INITIAL_POTIONS = 3;
+export const INITIAL_POTIONS = 2;
 
 export const COVER_AMOUNT = 5;
 export const ENCOUNTER_RATE = 0.1;
