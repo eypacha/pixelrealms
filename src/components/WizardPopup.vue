268d6f81325bcccca1d92ffef5e4572f952d8ccd
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-17 13:26:21 -0300

    feat: update font family to Pixelify Sans and enhance button cursor styles

--- src/components/WizardPopup.vue
+++ src/components/WizardPopup.vue
@@ -12,7 +12,7 @@
       <div class="mt-2 text-center text-sm text-gray-700 mb-5">{{ message }}</div>
 
       <div class="flex flex-col gap-2 items-center">
-        <button @click="usePotion" :disabled="playerStore.inventory.potion <= 0" class="px-4 py-1 w-full">Buy Potion: {{ POTION_COST }}🪙</button>
+        <button @click="usePotion" :disabled="playerStore.inventory.potion <= 0" class="px-4 py-1 w-full cursor-pointer">Buy Potion: {{ POTION_COST }}🪙</button>
         <button @click="enchantSword" class="px-4 py-1 w-full cursor-pointer">Enchant Sword: {{ ENCHANT_COST }}🪙</button>
         <button @click="enchantShield" class="px-4 py-1 w-full cursor-pointer">Enchant Shield: {{ ENCHANT_COST }}🪙</button>
         <button @click="closePopup" class="px-4 py-1 w-full mt-2 cursor-pointer">Leave</button>
