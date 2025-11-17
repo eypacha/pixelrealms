0d9f8cf7ab05c93cef86cf6175a3c681a3e620af
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-16 22:35:18 -0300

    fix: add cursor pointer style to buttons in WizardPopup for better user experience

--- src/components/WizardPopup.vue
+++ src/components/WizardPopup.vue
@@ -13,9 +13,9 @@
 
       <div class="flex flex-col gap-2 items-center">
         <button @click="usePotion" :disabled="playerStore.inventory.potion <= 0" class="px-4 py-1 w-full">Buy Potion: {{ POTION_COST }}🪙</button>
-        <button @click="enchantSword" class="px-4 py-1 w-full">Enchant Sword: {{ ENCHANT_COST }}🪙</button>
-        <button @click="enchantShield" class="px-4 py-1 w-full">Enchant Shield: {{ ENCHANT_COST }}🪙</button>
-        <button @click="closePopup" class="px-4 py-1 w-full mt-2">Leave</button>
+        <button @click="enchantSword" class="px-4 py-1 w-full cursor-pointer">Enchant Sword: {{ ENCHANT_COST }}🪙</button>
+        <button @click="enchantShield" class="px-4 py-1 w-full cursor-pointer">Enchant Shield: {{ ENCHANT_COST }}🪙</button>
+        <button @click="closePopup" class="px-4 py-1 w-full mt-2 cursor-pointer">Leave</button>
       </div>
     </div>
   </div>
