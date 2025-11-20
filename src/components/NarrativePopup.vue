44099a85da33626e9f5075e88dcbaab90578d3cd
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-20 14:42:32 -0300

    fix: add missing text-balance class to anecdote title in NarrativePopup

--- src/components/NarrativePopup.vue
+++ src/components/NarrativePopup.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="visible" class="absolute inset-0 bg-[#00000040] flex justify-center items-center z-50">
     <div class="bg-white p-5 text-center  w-100 h-80  flex flex-col justify-center items-center">
-      <h2 class="text-lg font-bold mb-2">{{ getAnecdote().title }}</h2>
+      <h2 class="text-lg font-bold mb-2 text-balance">{{ getAnecdote().title }}</h2>
       <p class="mt-2 text-center text-sm text-gray-700 mb-5">{{ getAnecdote().text }}</p>
       <div class="flex flex-col gap-2 items-center w-full">
         <button @click="closePopup" class="px-4 py-1 w-full mt-2 cursor-pointer text-black transition">{{ $t('treasure.continue') }}</button>
