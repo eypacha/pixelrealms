bf5e6ea156dd8184655b8ec6cfdd197164b1ef9e
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-20 21:25:09 -0300

    feat: update RECOVERY_STEPS constant to 5 for improved health recovery logic

--- src/constants/player.js
+++ src/constants/player.js
@@ -4,7 +4,7 @@ export const INITIAL_STRENGTH = 10;
 export const INITIAL_DEFENSE = 10;
 export const INITIAL_COINS = 10;
 export const INITIAL_POTIONS = 2;
-export const RECOVERY_STEPS = 3;
+export const RECOVERY_STEPS = 5;
 
 export const PLAYER_HIT_CHANCE = 0.7;
 
