94d10f2d8a6c5fec5ced13b6167160c5d2e82c8e
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-20 21:38:18 -0300

    feat: add ENEMY_PAUSE constant to player store for improved enemy behavior

--- src/constants/enemies.js
+++ src/constants/enemies.js
@@ -2,6 +2,7 @@ export const ENCOUNTER_RATE_DAY = 0.1;
 export const ENCOUNTER_RATE_NIGHT = 0.3;
 export const ENEMY_HIT_CHANCE = 0.7;
 
+export const ENEMY_PAUSE = 1000;
 export const GOBLIN = 'goblin';
 export const GOBLIN_HEALTH = 10;
 export const GOBLIN_STRENGTH = 8;
