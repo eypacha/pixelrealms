d319b48423a9ab0667cc45b55d877dd4e710c234
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-25 10:36:42 -0300

    fix: remove initial player stat constants to allow dynamic assignment

--- src/constants/player.js
+++ src/constants/player.js
@@ -1,11 +1,5 @@
 export const PLAYER_SPEED = 5;
 
-export const INITIAL_HEALTH = 10;
-export const INITIAL_STRENGTH = 10;
-export const INITIAL_DEFENSE = 10;
-export const INITIAL_COINS = 10;
-
-export const INITIAL_POTIONS = 2;
 export const RECOVERY_STEPS = 5;
 export const PLAYER_HIT_CHANCE = 0.7;
 export const COVER_AMOUNT = 5;
